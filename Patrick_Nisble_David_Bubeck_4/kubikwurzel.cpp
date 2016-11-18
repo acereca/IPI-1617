24f2b13a2d5cba9b1eabb6cd8fadee965c4e231c
acereca <tigerauge96@live.de>
2016-11-18 17:19:25 +0100

    start ex4

---
+++ Patrick_Nisble_David_Bubeck_4/kubikwurzel.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Patrick on 17.11.2016.
+//
+
+#include <cmath>
+
+double cbrt(double y);
+
+void main(){
+
+}
+
+double cbrt(double y){
+    double def = std::abs(y)*std::pow(10,-15);
+    return ;
+}
