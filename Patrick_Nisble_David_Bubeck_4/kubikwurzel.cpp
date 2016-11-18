dff2753027a7a2ea12054f22a7b72b5a5eeae97c
acereca <tigerauge96@live.de>
2016-11-18 22:12:28 +0100

    add 1 and 2

--- Patrick_Nisble_David_Bubeck_4/kubikwurzel.cpp
+++ Patrick_Nisble_David_Bubeck_4/kubikwurzel.cpp
@@ -1,16 +1,45 @@
 //
-// Created by Patrick on 17.11.2016.
+// Created by Patrick Nisble on 17.11.2016.
 //
 
 #include <cmath>
+#include <assert.h>
+#include <iostream>
 
+/// calculate the cubic root of y using Newtons algorithm
+/// \param y
+/// \return cuberoot of y, err = 10^-15 * y
 double cbrt(double y);
 
-void main(){
+int main(){
+
+    double list[] = {27,26,3,1293};
+
+    for (auto entr: list){
+        std::cout <<
+                  "the cbrt of " << entr <<
+                  " is " << cbrt(entr) <<
+                  " with an error of " << std::abs(cbrt(entr) - std::pow(entr, 1.0/3.0)) << "\n";
+
+        //assume error for algorithms of 10^-15*y
+        assert(std::abs(cbrt(entr)-std::pow(entr, 1.0/3.0)) <= std::pow(10,-15)*entr);
+    }
 
 }
 
 double cbrt(double y){
-    double def = std::abs(y)*std::pow(10,-15);
-    return ;
+
+    //error calculation
+    double dx = std::abs(y)*std::pow(10,-15);
+
+    //initial guess
+    double x = y;
+
+    //abortion requirement: as 1.2 in blatt4-beweise.pdf
+    while(std::abs(x*x*x-y) >= dx){
+
+        //incrementation rule: 1.1 in blatt4-beweise.pdf
+        x = (1.0/3.0)*(2*x+y/x/x);
+    }
+    return x;
 }
---
+++ Patrick_Nisble_David_Bubeck_4/pythagoras.cpp
@@ -0,0 +1,52 @@
+//
+// Created by Patrick Nisble on 18.11.2016.
+//
+
+#include <cmath>
+#include <iostream>
+
+/// checks if sqrt(n) is an int
+/// \param n to be checked
+/// \return true if sqrt(n) is int
+bool is_square(int n);
+
+/// printout all pythagorean triples with a^2+b^2=c^2, a<b
+/// \param bmax maximum value for b to check
+void pythagorean_triples(int bmax);
+
+int main(){
+    std::cout << "a^2\t+ b^2\t= c^2" << "\n" << "---------------------" << "\n";
+    pythagorean_triples(400);
+}
+
+bool is_square(int n){
+
+    //optimization: 2.1 in blatt4-beweise.pdf
+    if (n%4 == 0 or n%4 == 1) {
+
+        double sqrt = std::sqrt(n);
+
+        //try for sqrt(n) is an int
+        if ((sqrt - std::floor(sqrt)) == 0.0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void pythagorean_triples(int bmax) {
+
+    for (int b = 1; b <= bmax; b++ ) {
+        for (int a = 1; a < b; a++ ) {
+
+            int c = a*a+b*b;
+
+            
+            if (is_square(c)){
+                std::cout << a << "^2\t+ " << b << "^2\t= " << std::sqrt(c) << "^2" << "\n";
+            }
+        }
+
+    }
+}
+
